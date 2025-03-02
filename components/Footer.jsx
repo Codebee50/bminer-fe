4adfd3634b3b14b64f650baf13f1c1f2f1deb98b
Code bee <codebee345@outlook.com>
2025-03-03 00:22:53 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,113 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
+import {
+  FaFacebookF,
+  FaTelegramPlane,
+  FaLinkedinIn,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
+import { PiXBold } from "react-icons/pi"; // X (Twitter) icon from Phosphor Icons
 
 const Footer = () => {
+  const footerLinks = [
+    {
+      category: "Company",
+      links: [
+        { name: "About", url: "/about" },
+        { name: "Why Mining?", url: "/why-mining" },
+        { name: "Cloud Mining", url: "/cloud-mining" },
+        { name: "Blog", url: "/blog" },
+        { name: "Referral", url: "/referral" },
+        { name: "Contact", url: "/contact" },
+      ],
+    },
+    {
+      category: "Links",
+      links: [
+        { name: "Terms of use", url: "/terms-of-use" },
+        { name: "Privacy policy", url: "/privacy-policy" },
+        { name: "Support", url: "/support" },
+        { name: "Disclaimer", url: "/disclaimer" },
+        { name: "FAQ", url: "/faq" },
+      ],
+    },
+  ];
+
   return (
-    <SectionWrapper>
-      <footer className="flex flex-row items-start justify-between bg-red-300 ">
+    <footer className="w-full min-h-[60vh] flex flex-col bg-darkmuted border-t-[6px]  border-t-brightyellow">
+      <SectionWrapper>
         <div className="flex flex-col">
-          <img src="/logo.svg" alt="" />
+          <div className="flex flex-row items-start justify-between gap-4">
+            <div className="flex flex-col gap-4 max-w-[500px]">
+              <img src="/logowhite.svg" className="w-[100px]" alt="" />
+
+              <p className="text-[16px] text-[#b3b3b3]">
+                Trading Way FZCO - Dubai Silicon Oasis, PO Box 341041, Dubai,
+                UAE, Dubai Digital Park – Building A1
+              </p>
+              <div className="flex space-x-4">
+                {[
+                  { icon: FaFacebookF },
+                  { icon: FaTelegramPlane },
+                  { icon: FaLinkedinIn },
+                  { icon: PiXBold }, // Replacing FaXTwitter with PiXBold
+                  { icon: FaTiktok },
+                  { icon: FaYoutube },
+                ].map(({ icon: Icon }, index) => (
+                  <div
+                    key={index}
+                    className="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300 transition"
+                  >
+                    <Icon className="text-2xl" />
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {footerLinks.map((footerLink) => {
+              return (
+                <div
+                  className="flex flex-col gap-[10px]"
+                  key={footerLink.category}
+                >
+                  <p className="text-[18px] font-medium text-white">
+                    {footerLink.category}
+                  </p>
+                  {footerLink.links.map((link) => {
+                    return (
+                      <a
+                        className="text-[16px] text-[#b3b3b3]"
+                        href={link.url}
+                        key={link.name}
+                      >
+                        {link.name}
+                      </a>
+                    );
+                  })}
+                </div>
+              );
+            })}
+
+            <div className="flex flex-col gap-[10px]">
+              <p className="text-[18px] font-medium text-white">Contact</p>
+              <div className="flex flex-col">
+                <p className="text-[16px] text-[#b3b3b3]">E-mail:</p>
+                <a className="text-white ">official@1bitminner.com</a>
+              </div>
+              <div className="flex flex-col">
+                <p className="text-[16px] text-[#b3b3b3]">Call phone:</p>
+                <a className="text-white ">+13128204509</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="border-t border-t-grey400 mt-7 pt-[20px]">
+            <p className="text-[16px] text-[#b3b3b3]">© 2025 1BITUP. All rights reserved</p>
+          </div>
         </div>
-      </footer>
-    </SectionWrapper>
+      </SectionWrapper>
+    </footer>
   );
 };
 
